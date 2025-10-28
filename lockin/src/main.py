49105e2bd8e0aaf212a52fc533be4b8bf56f095c
ymcx <89810988+ymcx@users.noreply.github.com>
2025-10-28 14:48:36 +0200

    move submission to score parsing into parse.py, improve argument handling

--- lockin/src/main.py
+++ lockin/src/main.py
@@ -42,36 +42,46 @@ def get_reddit(credentials: dict[str, str]) -> Reddit:
     )
 
 
-def main() -> None:
-    credentials = read_credentials(sys.argv[1])
-    reddit = get_reddit(credentials)
-    figure, axes, lines = graph.create()
+def read_arguments() -> tuple[str, bool]:
+    match len(sys.argv):
+        case 1:
+            print("Please pass the credentials.txt file")
+            sys.exit()
+        case 2:
+            print("Please specify if you want to print the scores or show the graph")
+            sys.exit()
+        case _:
+            if sys.argv[2] != "graph" and sys.argv[2] != "text":
+                print("Invalid mode given, please select either 'graph' or 'text'")
+                sys.exit()
 
+    return sys.argv[1], sys.argv[2] == "graph"
+
+
+def main() -> None:
+    arguments = read_arguments()
+    credentials = read_credentials(arguments[0])
+    create_graph = arguments[1]
+
+    reddit = get_reddit(credentials)
     scores = defaultdict(defaultdict)
     subreddits = "+".join(SUBREDDITS)
     iteration = 0
 
+    if create_graph:
+        figure, axes, lines = graph.create()
+
     while True:
         submissions = reddit.subreddit(subreddits).new()
+        parse.scores(scores, submissions)
 
-        for submission in submissions:
-            id = submission.id
-            title = submission.title
-            comments = submission.num_comments
-            score = submission.score
-            epoch = submission.created_utc
-
-            tickers = parse.tickers(title)
-
-            for ticker in tickers:
-                scores[ticker][id] = (comments, score, epoch)
-
-        scores_str = format.scores_str(scores, AMOUNT_TO_LIST)
-        os.system("clear")
-        print(scores_str)
-
-        scores_data = format.scores_data(scores, AMOUNT_TO_LIST)
-        graph.update(iteration, scores_data, figure, axes, lines)
+        if create_graph:
+            scores_data = format.scores_data(scores, AMOUNT_TO_LIST)
+            graph.update(iteration, scores_data, figure, axes, lines)
+        else:
+            scores_str = format.scores_str(scores, AMOUNT_TO_LIST)
+            os.system("clear")
+            print(scores_str)
 
         iteration += 1
         time.sleep(60 * POLL_INTERVAL)
