5c81387cea5b77d8b72f619d27c293f5dde5dff4
ymcx <89810988+ymcx@users.noreply.github.com>
2025-11-04 00:51:27 +0200

    add a timer

--- lockin/src/lockin/logic.py
+++ lockin/src/lockin/logic.py
@@ -1,9 +1,34 @@
 from praw import Reddit
-from lockin import parse
+from lockin import parse, io
 from lockin.score import Scores
+from threading import Event, Thread
+import time
 
 
-def sync(reddit: Reddit, scores: Scores, subreddits: str) -> None:
+# https://stackoverflow.com/a/33054922
+class Timer:
+    def __init__(self, interval, function, *args) -> None:
+        self.interval = interval
+        self.function = function
+        self.args = args
+
+        self.start = time.time()
+        self.event = Event()
+
+        thread = Thread(target=self._target)
+        thread.start()
+
+        self.function(*self.args)
+
+    def _target(self) -> None:
+        while not self.event.wait(self._time()):
+            self.function(*self.args)
+
+    def _time(self) -> int:
+        return self.interval - (time.time() - self.start) % self.interval
+
+
+def _sync(reddit: Reddit, scores: Scores, subreddits: str) -> None:
     for submission in reddit.subreddit(subreddits).new():
         id = submission.id
         title = submission.title
@@ -13,3 +38,10 @@ def sync(reddit: Reddit, scores: Scores, subreddits: str) -> None:
 
         for ticker in parse.tickers(title):
             scores[ticker][id] = (comments, score, epoch)
+
+
+def thread(
+    reddit: Reddit, scores: Scores, subreddits: str, amount_to_list: int
+) -> None:
+    _sync(reddit, scores, subreddits)
+    io.list_scores(scores, amount_to_list)
--- lockin/src/lockin/main.py
+++ lockin/src/lockin/main.py
@@ -1,14 +1,10 @@
 from collections import defaultdict
-from lockin import logic, io
-import time
+from lockin import io, logic
+from lockin.logic import Timer
 
 
 def main() -> None:
     amount_to_list, poll_interval, subreddits, reddit = io.read_config()
     scores = defaultdict(defaultdict)
 
-    while True:
-        logic.sync(reddit, scores, subreddits)
-        io.list_scores(scores, amount_to_list)
-
-        time.sleep(60 * poll_interval)
+    Timer(poll_interval, logic.thread, reddit, scores, subreddits, amount_to_list)
